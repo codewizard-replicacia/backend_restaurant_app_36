a578708a3ba5e54343f3bce9cd622fc81115bcb9
Administrator <Administrator@172.31.36.201>
2023-09-07 10:44:00 +0000

    initial commit

---
+++ src/utils/display_configuration.js
@@ -0,0 +1,48 @@
+export const vendorViewConfig = {
+Details: [
+    {
+      key: "Vendor",
+      value: "Vendor",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "Category",
+      value: "Category",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "ProductName",
+      value: "ProductName",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "ProductUnitsPurchased",
+      value: "ProductUnitsPurchased",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "TotalUnitsPurchasedPrice",
+      value: "TotalUnitsPurchasedPrice",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "PricePerUnit",
+      value: "PricePerUnit",
+      type: "text",
+      editable: false,
+      required: true
+    },
+  ],
+Type: [
+  ],
+}
---
+++ src/views/vendor_details/vendor_view.js
@@ -0,0 +1,124 @@
+
+import { Box, Button, Checkbox, Divider, Grid, makeStyles, Table, TableBody, TableCell, TableRow, Typography } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router";
+import { BASE_URL, PATH_VENDOR } from "../../utils/constants";
+import makeApiCall from "../../utils/makeApiCall";
+import { vendorViewConfig } from "../../utils/display_configuration";
+import moment from "moment";
+
+const useStyles = makeStyles({
+  table: {
+    margin: "0 auto",
+    width: "90%",
+  },
+  titleCell: {
+    width: "35%",
+    textAlign: "right",
+    borderBottom: "none",
+  },
+  valueCell: {
+    textAlign: "left",
+    borderBottom: "none",
+  },
+});
+
+const ViewVendor = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const styles = useStyles();
+
+  const [vendor, setVendor] = useState({});
+
+  useEffect(() => {
+    if (id) {
+      const fetchVendorById = async () => {
+        const VendorResponse = await makeApiCall(
+          `${BASE_URL}${PATH_VENDOR}(${id})`
+        );
+        const VendorJsonResp = await VendorResponse.json();
+        setVendor(VendorJsonResp);
+      };
+      fetchVendorById();
+    }
+  }, [id]);
+  return (
+    <>
+      {vendor && (
+        <Box padding={2}>
+          <Grid>
+            <Grid item lg={12} xs={12}>
+              <Box display="flex" justifyContent="space-between">
+                <Typography className="page-heading" variant="h5">View Vendor</Typography>
+                <div  className="action-buttons">
+                
+                  <Button
+                    onClick={() => navigate('/Vendors')}
+                    variant="contained"
+                    color="secondary"
+                    size="small"
+                  >
+                    Back
+                  </Button>
+                </div>
+              </Box>
+            </Grid>
+            <Divider />
+            <Box marginTop={2} className="form-container">
+              <Grid container item lg={12} xs={12}>
+                {Object.keys(vendorViewConfig).map((config, ind) => (
+                  <>
+                    <Grid item lg={5} md={5} xs={12}>
+                      <Box marginTop={1}>
+                        <Typography variant="h6">{config}</Typography>
+                        <Table size="small" className={styles.table}>
+                          <TableBody>
+                            {vendorViewConfig[config].map(
+                              ({ key, value, type }) => (
+                                <TableRow key={key} className="responsive-table-row">
+                                  <TableCell className={[styles.titleCell, 'row-label'].join(' ')}>
+                                    <Typography variant="body1">
+                                      {value}:{" "}
+                                    </Typography>
+                                  </TableCell>
+                                  <TableCell className={[styles.valueCell, 'row-value'].join(' ')}>
+                                    {
+                                      type === "date" ? (
+                                      <Typography variant="body1">
+                                        {vendor[key] !== null &&
+                                          moment(vendor[key]).format(
+                                            "DD-MMMM-YYYY HH:mm:ss A"
+                                          )}
+                                      </Typography>
+                                    ) : 
+                                    type === "boolean" ? (
+                                      <Checkbox
+                                        checked={vendor[key] || false}
+                                        disabled
+                                      />
+                                    ) : (
+                                      <Typography variant="body1">
+                                        {vendor[key]}
+                                      </Typography>
+                                    )}
+                                  </TableCell>
+                                </TableRow>
+                              )
+                            )}
+                          </TableBody>
+                        </Table>
+                      </Box>
+                    </Grid>
+                    <Grid item lg={1} md={1} xs={false} />
+                  </>
+                ))}
+              </Grid>
+            </Box>
+          </Grid>
+        </Box>
+      )}
+    </>
+  );
+};
+
+export default ViewVendor;
